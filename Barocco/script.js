77be64faf923c82388bbec8236688c8cb49fa631
federico423 <76159231+federico423@users.noreply.github.com>
2021-01-22 14:50:02 +0100

    Add files via upload

---
+++ Barocco/script.js
@@ -0,0 +1,29 @@
+function slide1a2() {
+    document.getElementById('slider-foto1').style.display = 'none';
+    document.getElementById('slider-foto2').style.display = 'block';
+}
+
+function slide2a3() {
+    document.getElementById('slider-foto2').style.display = 'none';
+    document.getElementById('slider-foto3').style.display = 'block';
+}
+
+function slide3a1() {
+    document.getElementById('slider-foto3').style.display = 'none';
+    document.getElementById('slider-foto1').style.display = 'block';
+}
+
+function slide1a3() {
+    document.getElementById('slider-foto1').style.display = 'none';
+    document.getElementById('slider-foto3').style.display = 'block';
+}
+
+function slide3a2() {
+    document.getElementById('slider-foto3').style.display = 'none';
+    document.getElementById('slider-foto2').style.display = 'block';
+}
+
+function slide2a1() {
+    document.getElementById('slider-foto2').style.display = 'none';
+    document.getElementById('slider-foto1').style.display = 'block';
+}
