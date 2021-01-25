7e99847c20a4bccf28322cc7c65ddf9542ebb5fb
federico423 <76159231+federico423@users.noreply.github.com>
2021-01-25 20:35:00 +0100

    Add files via upload

--- Barocco/script.js
+++ Barocco/script.js
@@ -1,3 +1,4 @@
+//Comandi per lo slider
 function slide1a2() {
     document.getElementById('slider-foto1').style.display = 'none';
     document.getElementById('slider-foto2').style.display = 'block';
@@ -26,4 +27,119 @@ function slide3a2() {
 function slide2a1() {
     document.getElementById('slider-foto2').style.display = 'none';
     document.getElementById('slider-foto1').style.display = 'block';
+}
+
+//Comandi per caricare le altre immagini
+function bottone0() {
+    document.getElementById('foto0').style.display = 'block';
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone1() {
+    document.getElementById('foto0').style.display = 'none';
+    document.getElementById('foto1').style.display = 'block';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone2() {
+    document.getElementById('foto0').style.display = 'none';
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'block';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone3() {
+    document.getElementById('foto0').style.display = 'none';
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'block';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone4() {
+    document.getElementById('foto0').style.display = 'none';
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'block';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone5() {
+    document.getElementById('foto0').style.display = 'none';
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'block';
+}
+
+
+function bottone13() {
+    document.getElementById('foto1').style.display = 'block';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+}
+
+function bottone23() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'block';
+    document.getElementById('foto3').style.display = 'none';
+}
+
+function bottone33() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'block';
+}
+
+
+function bottone15() {
+    document.getElementById('foto1').style.display = 'block';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone25() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'block';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone35() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'block';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone45() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'block';
+    document.getElementById('foto5').style.display = 'none';
+}
+
+function bottone55() {
+    document.getElementById('foto1').style.display = 'none';
+    document.getElementById('foto2').style.display = 'none';
+    document.getElementById('foto3').style.display = 'none';
+    document.getElementById('foto4').style.display = 'none';
+    document.getElementById('foto5').style.display = 'block';
 }
